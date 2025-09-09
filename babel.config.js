de5db3adc86745f15cb2cadad74984d101e71b7c
Hemant Pal <hemantpal2004@gmail.com>
2025-09-09 11:14:44 +0530

    Move React Native app to root

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@/components': './src/components',
+          '@/screens': './src/screens',
+          '@/redux': './src/redux',
+          '@/services': './src/services',
+          '@/types': './src/types',
+          '@/utils': './src/utils',
+          '@/constants': './src/constants',
+        },
+      },
+    ],
+  ],
+};
